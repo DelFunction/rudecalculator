f9f1699e06ef5ceee2d3c2c9dfb1fa80f2605eba
DelFunction <120332967+DelFunction@users.noreply.github.com>
2024-02-03 22:47:28 +0000

    rudegeekycalculator.py
    supports number withs freakin decimals in them

--- simple calculator.py
+++ simple calculator.py
@@ -1,15 +1,15 @@
-##CALCULATOR
-print("MY FUCKING CALCULATOR!!!!!!!! YOU BETTER LIKE IT11!!")
+##CALCULATOR MADE BY SOME F**CKING GEEK ON GITHUB!!
+print("MY F*CKING CALCULATOR!!!!!!!! YOU BETTER LIKE IT11!!")
 first = input("First:")
 second = input("Second:")
 
 sum = float(first) + float(second)
 print("Sum: " + str(sum))
-print("DID YOU FUCKING LIKE IT>>???!!11")
+print("DID YOU F*CKING LIKE IT>>???!!11")
 answer = input()
 
 if answer == "yes":
-    print("GOOD BOY, YOU WILL NOT GET FUCKING SCREAMED AT TODAY.")
+    print("GOOD BOY, YOU WILL NOT GET F*CKING SCREAMED AT TODAY")
 elif answer == "no":
     while 3 > 2:
-     print("RAGHHHHH!!! FUCK YOU NOOB!!! FACE MY WRATH!!!")
+     print("RAGHHHHH!!! F*CK YOU NOOB!!! FACE MY WRATH!!!")
